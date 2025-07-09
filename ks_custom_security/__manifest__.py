c7753d7f275fb3448821bd8372ecc4db0eaf8069
mantej-ksolves <mantej.hanspal@ksolves.com>
2025-07-09 17:04:47 +0530

    kkg_21_employee_customization

---
+++ ks_custom_security/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Ks Custom Security',
+    'version': '18.0.1.0.0',
+    'summary': 'Customizations related to Security Groups',
+    'description': 'Customizations related to purchase app',
+    'author': 'Ksolves India Ltd.',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'security/security.xml'
+    ],
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': True,
+}
