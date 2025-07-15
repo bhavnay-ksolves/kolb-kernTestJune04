436e475ba3c4fa7f69fd774b3cb3ab95c3faebee
mantejks237 <mantej.hanspal@ksolves.com>
2025-07-15 16:34:49 +0530

    1.[IMP] - Updated field type for 'Rental free Time' in measurement_calculation_line. 2.[ADD] - Added  Measurement calculation relation with account.move.

--- ks_sales_extend/__manifest__.py
+++ ks_sales_extend/__manifest__.py
@@ -7,7 +7,7 @@
                "2. Measurement Calculation"
                "3. Add GEAB option on Sales."
                "4. Print flag on product and sales order line",
-    "depends": ['base',"sale_management", 'sale_project', "product"],
+    "depends": ['base','sale_management', 'sale_project', 'product','account'],
     "data": [
         'security/ir.model.access.csv',
         "data/product_data.xml",
@@ -17,7 +17,8 @@
         "views/offer_type.xml",
         "views/measurement_calulation_view.xml",
         "views/measurement_calculation_subtotal_view.xml",
-        "views/project_project_view.xml"
+        "views/project_project_view.xml",
+        "views/account_move_view.xml"
     ],
     "installable": True,
     "application": True
