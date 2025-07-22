ce239ead424d34f6de186a18b445e7c0240546fd
mantejks237 <mantej.hanspal@ksolves.com>
2025-07-22 15:00:47 +0530

    Enhance documentation and structure across multiple models and views

--- ks_gaeb/models/sale_order.py
+++ ks_gaeb/models/sale_order.py
@@ -1,12 +1,26 @@
 # -*- coding: utf-8 -*-
+"""
+Extension of the Sale Order model to add functionality for downloading GAEB files.
+
+This module defines methods for generating and downloading GAEB files in .x86 and .d86 formats.
+It uses QWeb templates to render the file content and creates attachments for download.
+
+Classes:
+    SaleOrder: Extends the `sale.order` model to include GAEB file download functionality.
+
+Attributes:
+    ks_name (Char): Custom field added to the Sale Order model.
+
+Methods:
+    action_download_x86_file: Generates and downloads a GAEB .x86 file for the sale order.
+    action_download_d86_file: Generates and downloads a GAEB .d86 file for the sale order.
+"""
 
 import base64
 import logging
 from datetime import datetime
 
 from odoo.http import request
-
-from odoo import models, fields
 from odoo import models, fields, api, exceptions
 
 _logger = logging.getLogger(__name__)
@@ -14,11 +28,24 @@ THUNDERSTORM_WEATHER_CODES = [95, 96, 97, 98, 99]
 
 
 class SaleOrder(models.Model):
+    """
+    Extension of the Sale Order model to add GAEB file download functionality.
+    """
     _inherit = 'sale.order'
 
+    # Custom field added to the Sale Order model
     ks_name = fields.Char()
 
     def action_download_x86_file(self):
+        """
+        Generates and downloads a GAEB .x86 file for the sale order.
+
+        This method renders a QWeb template to generate the XML content of the .x86 file,
+        creates an attachment for the file, and returns an action to download it.
+
+        Returns:
+            dict: Action to download the generated .x86 file.
+        """
         self.ensure_one()
         now = datetime.now()
 
@@ -51,6 +78,15 @@ class SaleOrder(models.Model):
         }
 
     def action_download_d86_file(self):
+        """
+        Generates and downloads a GAEB .d86 file for the sale order.
+
+        This method renders a QWeb template to generate the text content of the .d86 file,
+        creates an attachment for the file, and returns an action to download it.
+
+        Returns:
+            dict: Action to download the generated .d86 file.
+        """
         self.ensure_one()
 
         currency = self.currency_id.name if self.currency_id else 'EUR'
