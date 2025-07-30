b44f4df06402dbdc86db22a4cd21c44e51d85ce9
mantejks237 <mantej.hanspal@ksolves.com>
2025-07-30 18:23:25 +0530

    1. [Update] - Made project field mandatory on sale order 2. [Update] - Brought back task code

--- ks_project_extend/models/task.py
+++ ks_project_extend/models/task.py
@@ -9,18 +9,18 @@ class ProjectTask(models.Model):
     # weather_ids = fields.One2many('project.task.weather', 'task_id', string="Weather Reports")
     ks_weather = fields.Selection(selection=[('bad', 'Bad'),('clear', 'Clear Skies')],string="Weather",default='clear',tracking=True)
 
-    # def action_fetch_weather(self):
-    #     self.ensure_one()
-    #     if not self.project_id.latitude or not self.project_id.longitude:
-    #         raise UserError("Project has no latitude or longitude configured.")
-    #
-    #     return {
-    #         'type': 'ir.actions.act_window',
-    #         'name': 'Fetch Weather',
-    #         'res_model': 'project.weather.fetch.wizard',
-    #         'view_mode': 'form',
-    #         'target': 'new',
-    #         'context': {
-    #             'default_task_id': self.id,
-    #         },
-    #     }
+    def action_fetch_weather(self):
+        self.ensure_one()
+        if not self.project_id.latitude or not self.project_id.longitude:
+            raise UserError("Project has no latitude or longitude configured.")
+
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Fetch Weather',
+            'res_model': 'project.weather.fetch.wizard',
+            'view_mode': 'form',
+            'target': 'new',
+            'context': {
+                'default_task_id': self.id,
+            },
+        }
